90fca4b7c4e3bc4131bd061b2d769e95aa32d6e9
Ali yousefian <aliysefian@gmail.com>
2022-03-17 12:58:02 +0330

    fix orderby on last date added

--- db/crud.py
+++ db/crud.py
@@ -20,7 +20,7 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def get_posts(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Posts).offset(skip).limit(limit).all()
+    return db.query(models.Posts).order_by(models.Posts.Local_modified.desc()).offset(skip).limit(limit).all()
 
 
 def create_user(db: Session, user: schemas.UserCreate):
